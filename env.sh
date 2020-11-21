18061eb56bfa505af3a0cab76d157782f454abde
jiong <fatjyc@gmail.com>
2020-11-21 18:59:03 +0800

    fix env.sh

--- env.sh
+++ env.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # line endings must be \n, not \r\n !
 echo "window._env_ = {" > ./env.js
-awk -F '=' '{ print $1 ": \"" (ENVIRON[$1] ? ENVIRON[$1] : $2) "\"," }' ./.env >> ./env.js
+awk -F '=' '{ print $1 ": \"" (ENVIRON[$1]) "\"," }' ./.env >> ./env.js
 echo "}" >> ./env.js

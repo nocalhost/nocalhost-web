3ccb923b6811a70045284983a0daab0c2ec9e6ba
jiong <fatjyc@gmail.com>
2020-12-10 19:26:48 +0800

    update theme color

--- src/theme.ts
+++ src/theme.ts
@@ -2,17 +2,17 @@ import { createMuiTheme } from '@material-ui/core/styles';
 
 const NhTheme = createMuiTheme({
     palette: {
-        primary: {
-            light: '#767de8',
-            main: '#4051b5',
-            dark: '#002984',
-            contrastText: '#fff',
-        },
         secondary: {
-            light: '#767de8',
-            main: '#4051b5',
-            dark: '#002984',
-            contrastText: '#fff',
+            main: '#0080ff',
+            light: '#69afff',
+            dark: '#0055cb',
+            contrastText: '#ffffff',
+        },
+        primary: {
+            main: '#6f38ff',
+            light: '#8b5fff',
+            dark: '#4d27b2',
+            contrastText: '#ffffff',
         },
     },
 });

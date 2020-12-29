dd29dad60748e743218805fd6cf156dfe3aa39b0
jiong <fatjyc@gmail.com>
2020-12-29 11:40:01 +0800

    modified cluster storageclass

--- src/cluster/storage-class-input.tsx
+++ src/cluster/storage-class-input.tsx
@@ -1,13 +1,13 @@
 import React, { FC, useEffect, useState } from 'react';
-import { fetchUtils, SelectInput } from 'react-admin';
-import { Base64 } from 'js-base64';
+import { fetchUtils, SelectInput, Loading } from 'react-admin';
+import { Cluster, Result } from '../types';
 
 interface StorageClassInputProps {
-    formData: any;
+    formData: Cluster;
 }
 
 const StorageClassInput: FC<StorageClassInputProps> = (props: StorageClassInputProps) => {
-    const EMPTY_ITEM = { id: '', name: 'Do not use' };
+    const EMPTY_ITEM = { id: '', name: 'Default' };
     const [list, setList] = useState([]);
 
     const fetchStorageClassList = async () => {
@@ -21,7 +21,7 @@ const StorageClassInput: FC<StorageClassInputProps> = (props: StorageClassInputP
         let url;
         if (props.formData.id) {
             url = `http://${apiUrl}/v1/cluster/${props.formData.id}/storage_class`;
-            const result = await fetchUtils.fetchJson(url, options).then((result: any) => {
+            const result = await fetchUtils.fetchJson(url, options).then((result: Result) => {
                 if (result.json.code === 0) {
                     const list = result.json.data.type_name || [];
                     return list.map((item: string) => ({ id: item, name: item }));
@@ -31,39 +31,23 @@ const StorageClassInput: FC<StorageClassInputProps> = (props: StorageClassInputP
             });
             result.unshift(EMPTY_ITEM);
             setList(result);
-        } else {
-            url = `http://${apiUrl}/v1/cluster/kubeconfig/storage_class`;
-            const result = await fetchUtils
-                .fetchJson(url, {
-                    method: 'POST',
-                    body: JSON.stringify({
-                        kubeconfig: Base64.encode(props.formData.kubeconfig, false),
-                    }),
-                    ...options,
-                })
-                .then((result: any) => {
-                    if (result.json.code === 0) {
-                        const list = result.json.data.type_name || [];
-                        return list.map((item: string) => ({ id: item, name: item }));
-                    } else {
-                        return [];
-                    }
-                });
-            result.unshift(EMPTY_ITEM);
-            setList(result);
         }
     };
 
     useEffect(() => {
         fetchStorageClassList();
-    }, [props.formData]);
-    return (
-        <SelectInput
-            source="storage_class"
-            label="resources.cluster.fields.storage_class"
-            choices={list}
-        />
-    );
+    }, [props.formData.id]);
+    if (list && list.length > 0) {
+        return (
+            <SelectInput
+                source="storage_class"
+                label="resources.cluster.fields.storage_class"
+                choices={list}
+            />
+        );
+    } else {
+        return <Loading />;
+    }
 };
 
 export default StorageClassInput;

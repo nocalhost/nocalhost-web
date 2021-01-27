66a8aef0e5f2b5bd2322fc5e4e3451183f9ec411
liangguangming <gmliangd@163.com>
2021-01-27 11:10:54 +0800

    fix(api): when token expired, redirect to login page

--- src/provider/dataProvider.tsx
+++ src/provider/dataProvider.tsx
@@ -1,7 +1,15 @@
 import { fetchUtils, DataProvider } from 'react-admin';
 import data from './data';
 
-export default (apiUrl: string, httpClient = fetchUtils.fetchJson): DataProvider => {
+async function fetchJson(url: any, options?: fetchUtils.Options | undefined) {
+    const res = await fetchUtils.fetchJson(url, options);
+    if (res && res.json && res.json.code === 20103) {
+        return Promise.reject({ status: 403, msg: 'invalid token' });
+    }
+    return res;
+}
+
+export default (apiUrl: string, httpClient = fetchJson): DataProvider => {
     return {
         getList: (resource, params) => data.getList(apiUrl, httpClient, resource, params),
         getOne: (resource, params) => data.getOne(apiUrl, httpClient, resource, params),

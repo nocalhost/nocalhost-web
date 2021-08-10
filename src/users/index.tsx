cfe48d9e120c9f1336554969a565b15872c72c57
chuckie104 <296115103@qq.com>
2021-08-10 18:45:16 +0800

    delete admin

--- src/users/index.tsx
+++
@@ -1,9 +0,0 @@
-import UserIcon from '@material-ui/icons/People';
-
-import UserList from './list';
-import UserCreate from './create';
-import UserEdit from './edit';
-
-const resource = { list: UserList, create: UserCreate, edit: UserEdit, icon: UserIcon };
-
-export default resource;

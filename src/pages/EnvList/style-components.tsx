8a7f6adca642bbb5b6699e04e65d35c285167be8
yuanganping <yuanganping@coding.net>
2021-08-17 21:08:22 +0800

    style: UserName

--- src/pages/EnvList/style-components.tsx
+++ src/pages/EnvList/style-components.tsx
@@ -103,7 +103,7 @@ export const UserName = styled.div`
     font-size: 14px;
     font-family: PingFangSC-Regular;
     font-weight: normal;
-    text-align: justify;
+    word-break: break-word;
 `;
 
 export const Line1Px = styled.div``;

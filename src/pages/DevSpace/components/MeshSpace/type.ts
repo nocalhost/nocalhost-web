522f27c2a5c059868d1ced66d0cf6f4160ec33ee
chuckie104 <296115103@qq.com>
2021-08-27 15:38:33 +0800

    fix common animation time

--- src/pages/DevSpace/components/MeshSpace/type.ts
+++ src/pages/DevSpace/components/MeshSpace/type.ts
@@ -16,6 +16,7 @@ export interface ContentStyleProps {
     selectBoxWidth: number;
     selectBoxHeight: number;
     workLoadWidth: number;
+    animationTime: any;
 }
 
 export interface SpaceIconStyleProps {
